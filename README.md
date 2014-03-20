31d0863fd6865a09a2cfa64022de3fe30a69dd3f
Deandre Gibson <DGibson@chicagotechacademy.org>
2014-03-20 14:28:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Rails in 20mins: Third App
+
+This is the Third app in Rails in 20mins series by [Deandre Gibson](http://www.DeandreGibson.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirdApp::Application.config.secret_token = '4ec40890538575e8943deee4aa48407bdbf847b05ebf9c4390e72ea96f6c6a5f8906168228e31382f007a85e9a6447440add39e5e248b0d86e535ac13ef4d738'
